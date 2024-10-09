350e18e6cc0c49dbd7829a692508ce85a3f2307f
aasifrj <aasif@complyance.io>
2024-10-09 17:17:02 +0530

    Revert "Update page.js"
    This reverts commit b33c4f3cbb3bc73ff441a7921cc37fc487a3dbe9.

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,17 @@
-import { allBlogs } from "contentlayer/generated";
+import {allBlogs} from "contentlayer/generated";
 import HomeCoverSection from "../components/Home/HomeCoverSection";
 import FeaturedPosts from "../components/Home/FeaturedPosts";
 import RecentPosts from "../components/Home/RecentPosts";
 
 export default function Home() {
+  
   return (
     <main className="flex flex-col items-center justify-center">
       <HomeCoverSection blogs={allBlogs} />
       <FeaturedPosts blogs={allBlogs} />
       <RecentPosts blogs={allBlogs} />
-      hi
+
+
     </main>
-  );
+  )
 }
